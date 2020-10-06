24dd0271eac90b7fa75997bc8983d693a4439d70
vdanylov <vitaliidanylov1992@gmail.com>
2020-10-06 22:21:35 +0200

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ import { PreviewData, PreviewDataImage, Size } from './types'
 export const getActualImageUrl = (baseUrl: string, imageUrl?: string) => {
   let actualImageUrl = imageUrl?.trim()
   if (!actualImageUrl || actualImageUrl.startsWith('data')) return
-
+  // Put it in enum all these cases, it allows You to have different true options as an object with proper output
   if (actualImageUrl.startsWith('//'))
     actualImageUrl = `https:${actualImageUrl}`
 
@@ -19,6 +19,7 @@ export const getActualImageUrl = (baseUrl: string, imageUrl?: string) => {
     }
   }
 
+  // if(!['.png','.jpg','.jpeg','.bmp','.gif','.webp','.psd','.ico'].some(actualImageUrl?.endsWith) return `${actualImageUrl}.png`
   if (
     ![
       '.png',
@@ -36,13 +37,24 @@ export const getActualImageUrl = (baseUrl: string, imageUrl?: string) => {
   return actualImageUrl
 }
 
-export const getActualText = (text?: string) => {
+// export const getActualText = (text = '') => {
+//  const actualText = text.trim()
+//  return actualText ? AllHtmlEntities.decode(actualText) : undefined
+// }
+export const getActualText = (text?: string, defaultText?: string) => {
   const actualText = text?.trim()
-  if (!actualText) return
+  if (!actualText) return defaultText
 
   return AllHtmlEntities.decode(actualText)
 }
 
+// export const getContent = (left: string, right: string, type: string) => {
+//  const contents = {
+//      [left.trim()]: right,
+//      [right.trim()]: left
+//  }
+//  return contents[type]?.trim
+// }
 export const getContent = (left: string, right: string, type: string) => {
   if (left.trim() === type) return right.trim()
   if (right.trim() === type) return left.trim()
@@ -80,6 +92,7 @@ export const getPreviewData = async (text: string) => {
 
     const contentType = response.headers.get('content-type') ?? ''
 
+    // If you use RegExp without and you don't need return value just use test instead exec
     if (new RegExp('image/*', 'g').exec(contentType)) {
       const image = await getPreviewDataImage(link)
       previewData.image = image
@@ -115,24 +128,25 @@ export const getPreviewData = async (text: string) => {
       (acc, curr) => {
         // Verify that we have property/name and content
         // Note that if a page will specify property, name and content in the same meta, regex will fail
-        if (!curr[2] || !curr[4]) return acc
-
-        // Only take the first occurrence
+        const SECOND = curr[2]
+        const FOURTH = curr[4]
+        if (!SECOND || !FOURTH) return acc
+        // Too much usage of arrray[index], just move the constants        
+        // Only take the first occurrence const SECOND = curr[2]; const FOURTH = curr[4] and use it through the function
         // For description take the meta description tag into consideration
         const description =
           !acc.description &&
-          (getContent(curr[2], curr[4], 'og:description') ||
-            getContent(curr[2], curr[4], 'description'))
+          (getContent(SECOND, FOURTH, 'og:description') ||
+            getContent(SECOND, FOURTH, 'description'))
         const ogImage =
-          !acc.imageUrl && getContent(curr[2], curr[4], 'og:image')
-        const ogTitle = !acc.title && getContent(curr[2], curr[4], 'og:title')
+          !acc.imageUrl && getContent(SECOND, FOURTH, 'og:image')
+        const ogTitle = !acc.title && getContent(SECOND, FOURTH, 'og:title')
 
         return {
-          description: description
-            ? getActualText(description)
-            : acc.description,
+          // Changed
+          description: getActualText(description, acc.description),
           imageUrl: ogImage ? getActualImageUrl(link, ogImage) : acc.imageUrl,
-          title: ogTitle ? getActualText(ogTitle) : acc.title,
+          title: getActualText(ogTitle, acc.title),
         }
       },
       { title: previewData.title }
@@ -143,6 +157,7 @@ export const getPreviewData = async (text: string) => {
     previewData.title = metaPreviewData.title
 
     if (!previewData.image) {
+      // Move RegExp to the variables with understandable meaning (all of it)
       const imageSrc = new RegExp(
         '<link.*?rel=["\']image_src["\'].*?href=["\'](.*?)["\'].*?>',
         'g'
@@ -161,6 +176,9 @@ export const getPreviewData = async (text: string) => {
 
       let images: PreviewDataImage[] = []
 
+      // Please check if it's w hat You are trying achieve
+      // Promise.all(imgTags.map((tag) => getPreviewDataImage(getActualImageUrl(link, tag[1])))).then(res => images = res.filter(Boolean))
+
       for (const tag of imgTags) {
         const image = await getPreviewDataImage(getActualImageUrl(link, tag[1]))
 
@@ -176,27 +194,29 @@ export const getPreviewData = async (text: string) => {
 
     return previewData
   } catch {
+    // A bit confusing
     return previewData
   }
 }
 
 export const getPreviewDataImage = async (url?: string) => {
-  if (!url) return
-
-  try {
-    const { height, width } = await getImageSize(url)
-    const aspectRatio = height > 0 ? width / height : 1
-
-    if (height > 100 && width > 100 && aspectRatio > 0.1 && aspectRatio < 10) {
-      return {
-        height,
-        url,
-        width,
+    if (!url) return
+  
+    try {
+      const { height, width } = await getImageSize(url)
+      const aspectRatio = height > 0 ? width / height : 1
+  
+      if (height > 100 && width > 100 && aspectRatio > 0.1 && aspectRatio < 10) {
+        return {
+          height,
+          url,
+          width,
+        }
       }
+    } catch {
+        // Is this return for typescript
+      return
     }
-  } catch {
-    return
   }
-}
 
 export const LINK_REGEX = /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._+~#=]{1,256}\.(xn--)?[a-z0-9-]{2,20}\b([-a-zA-Z0-9@:%_+[\],.~#?&/=]*[-a-zA-Z0-9@:%_+\]~#?&/=])*/i
